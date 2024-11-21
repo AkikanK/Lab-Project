cc618c72c01f0374a2a7e9e44ae604d3e9e8295f
Kankaanpää Aki A S <kankaki@cubbli22-gold.local>
2024-11-21 21:11:55 +0200

    main.py

--- code/main.py
+++ code/main.py
@@ -8,19 +8,18 @@ pick = ["Rock", "Paper", "Scissors"]
     
 
 if __name__ == "__main__":
-    counter = 1
     M = Mark1()
     g = Game()
+    test = input("Want to play? Y/N")
+    if test == "n":
+        print("'Kay bye!")
+        False
+        
     while True:
-        test = input("Want to play? Y/N")
-        if test == "n":
-            print("'Kay bye!")
-            break
         player = int(input("Rock (0), Paper (1), or Scissors (2)? \n"))
         computer = M.current_propb()
         M.current = computer
         print(g.gameplay(player, computer))
-        print(f"Current Score: Player: {g.pscore}, Computer: {g.cscore}")
+        print(f"Current Score: Player: {g.pscore}, Computer: {g.cscore}, You've played {M.counter} rounds!")
         M.push(player)
         M.past_propb()
-        print(M.counter)
